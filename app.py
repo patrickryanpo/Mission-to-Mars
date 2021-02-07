56d1780adf47cfb5522095b84409d2048f623e91
patrickryanpo <73608741+patrickryanpo@users.noreply.github.com>
2021-02-07 16:14:34 +0800

    Deliverable 1 Update

---
+++ app.py
@@ -0,0 +1,49 @@
+# Import dependencies 
+from flask import Flask, render_template, request, redirect
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection 
+app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_app' #URI we'll be using to connect out Mongo app
+mongo = PyMongo(app) 
+
+# Set up out flask routes 
+# @app.route("/") tells Flask what to display when we're looking at the home page, index.html
+# when we visit our web app's HTML page, we will see the home page
+@app.route("/")
+def index():
+    # uses PyMongo to find the "mars" collection in our database, which
+    #we will create when we convert our Jupyter scraping code to Python
+    mars = mongo.db.mars.find_one()
+    # tells Flast to run an HTML template using an index.html file 
+    # mars=mars tells Python to use the "mars" collection in MongoDB
+    return render_template("index.html", mars=mars)
+
+# Function that will set up our scraping route. 
+# This route will be the "button" of the web application that will scrape updated data 
+
+# defines the route that Flask will be using
+@app.route("/scrape")
+def scrape():
+    # assign a new variable that points to our Mongo database
+    mars = mongo.db.mars
+    # create a new variable to hold the newly scraped data
+    mars_data = scraping.scrape_all()
+    # after gathering data, we need to update the database using .update()
+    # .update(query_parameter, data, options "upsert=True" means that we create a new document)
+    mars.update({}, mars_data, upsert=True)
+    #add a redirect after successfully scraping the data, navigate our page back to /
+    return redirect('/', code=302)
+
+# Tell Flask to run
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
---
+++ scraping.py
@@ -0,0 +1,104 @@
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=True)
+
+# initialize the browser, create a data dictionary, end the webdriver and return the scraped data. 
+def scrape_all():
+    #initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    # This dictionary does two things: It runs all of the functions we've created—featured_image(browser), for example—and it also stores all of the results. 
+    # When we create the HTML template, we'll create paths to the dictionary's values, which lets us present our data on our template. 
+    # We're also adding the date the code was run last by adding "last_modified": dt.datetime.now(). 
+    # For this line to work correctly, we'll also need to add import datetime as dt to our imported dependencies at the beginning of our code.
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    #Stop webdriver and return data
+    browser.quit()
+    return data
+
+# Define the function
+
+# we will be using the browser variable we defined outside the function. 
+# All of out scraping code utlizes an automated browser
+
+def mars_news(browser):
+
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    # Try portion right before the scraping:
+    #Telling Python to look for these elements, if there is an error
+    # instead of returning the title and paragraph, Nothing will be returned
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+        #slide_elem.find("div", class_='content_title')
+
+        # Use the parent element to find the first a tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
+    return news_title, news_p
+# ## JPL Space Images Featured Image
+
+# Declare and define our function
+def featured_image(browser):
+#10.3.4
+#Visit Archived JPL URL
+    try:
+        PREFIX = "https://web.archive.org/web/20181114023740"
+        url = f'{PREFIX}/https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(url)
+        article = browser.find_by_tag('article').first['style']
+        article_background = article.split("_/")[1].replace('");', "")
+        return(f'{PREFIX}_if/{article_background}')
+    except:
+        return('https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22486-main.jpg')
+# ## Mars Facts
+def mars_facts():
+    try:
+        #use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+    # BaseException is a bit of catchall when it comes to error handling
+    # Raised when any of the built-in exceptions are encountered
+    # It wont handle any user-defined exceptions 
+    except BaseException:
+        return None
+
+    df.columns = ['Description', 'Mars']
+    df.set_index('Description', inplace=True)
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html(classes="table table-striped")
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
